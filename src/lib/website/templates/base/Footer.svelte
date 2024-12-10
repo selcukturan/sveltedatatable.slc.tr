a710e32bc3714a076ab94b96d750a94e7ec4792a
Selcuk Turan <selcuk.turann@outlook.com>
2024-12-10 17:40:37 +0300

    İlk linux commit.

--- src/lib/website/templates/base/Footer.svelte
+++ src/lib/website/templates/base/Footer.svelte
@@ -7,5 +7,5 @@
 	class="bg-background/95 supports-[backdrop-filter]:bg-background/60 border-t border-border/70 p-4 backdrop-blur"
 >
 	<!-- {@render children?.()} -->
-	(v.0.0.7)
+	(v.0.0.8)
 </footer>
