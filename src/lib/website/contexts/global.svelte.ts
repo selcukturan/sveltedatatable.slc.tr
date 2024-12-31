db568fd4d1d07b6084904b9098a6f2728d77c40c
Selçuk TURAN - İŞ <selcuk.turann@outlook.com>
2024-12-31 15:31:57 +0300

    versiyon güncellendi.

--- src/lib/website/contexts/global.svelte.ts
+++ src/lib/website/contexts/global.svelte.ts
@@ -18,7 +18,7 @@ class GlobalContext {
 		appName: 'SLC Svelte Virtual Data Table',
 		windowWidth: 1000,
 		currentScreen: 'lg',
-		tableVersion: 'v0.0.1-44',
+		tableVersion: 'v0.0.1-45',
 		screens: {
 			sm: 640, // tablet
 			md: 768, // tablet landscape
