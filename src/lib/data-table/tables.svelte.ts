90feef3185b0a5131fb5d7276b914090fb1a8c45
Selçuk TURAN - İŞ <selcuk.turann@outlook.com>
2025-02-07 20:03:45 +0300

    v0.0.1-130 - Kod düzenlemesi yapıldı.

--- src/lib/data-table/tables.svelte.ts
+++ src/lib/data-table/tables.svelte.ts
@@ -6,7 +6,7 @@ class Table<TData extends Row> {
 	// ################################## BEGIN Constructor ################################################################
 	element?: HTMLDivElement = $state();
 	private set: Sources<TData> = $state({ id: '', columns: [] }); // orjinal sources. kaynaklar/sources değiştirilirken bu değişken kullanılacak. `table.set`
-	sources = (value: Sources<TData>) => (this.set = value); // Set All Sources Method
+	readonly sources = (value: Sources<TData>) => (this.set = value); // Set All Sources Method
 	constructor(sources: Sources<TData>) {
 		this.sources(sources);
 	}
@@ -30,37 +30,37 @@ class Table<TData extends Row> {
 	// ################################## END Default Sources ##############################################################
 
 	// ################################## BEGIN Set Sources ################################################################
-	id = (value: RequiredSources<TData>['id']) => (this.set.id = value);
-	data = (value: RequiredSources<TData>['data']) => {
+	readonly id = (value: RequiredSources<TData>['id']) => (this.set.id = value);
+	readonly data = (value: RequiredSources<TData>['data']) => {
 		this.clearSelectedRows();
 		this.clearFocusedCell();
 		this.set.data = value;
 	};
-	width = (value: RequiredSources<TData>['width']) => (this.set.width = value);
-	height = (value: RequiredSources<TData>['height']) => (this.set.height = value);
-	enableVirtualization = (value: RequiredSources<TData>['enableVirtualization']) => {
+	readonly width = (value: RequiredSources<TData>['width']) => (this.set.width = value);
+	readonly height = (value: RequiredSources<TData>['height']) => (this.set.height = value);
+	readonly enableVirtualization = (value: RequiredSources<TData>['enableVirtualization']) => {
 		this.clearSelectedRows();
 		this.clearFocusedCell();
 		this.set.enableVirtualization = value;
 	};
-	rowSelection = (value: RequiredSources<TData>['rowSelection']) => {
+	readonly rowSelection = (value: RequiredSources<TData>['rowSelection']) => {
 		this.clearSelectedRows();
 		this.clearFocusedCell();
 		this.set.rowSelection = value;
 	};
-	rowSelectionColumnWidth = (value: RequiredSources<TData>['rowSelectionColumnWidth']) => (this.set.rowSelectionColumnWidth = value);
-	theadRowHeight = (value: RequiredSources<TData>['theadRowHeight']) => (this.set.theadRowHeight = value);
-	tbodyRowHeight = (value: RequiredSources<TData>['tbodyRowHeight']) => (this.set.tbodyRowHeight = value);
-	tfootRowHeight = (value: RequiredSources<TData>['tfootRowHeight']) => (this.set.tfootRowHeight = value);
-	columns = (value: RequiredSources<TData>['columns']) => (this.set.columns = value);
-	footers = (value: RequiredSources<TData>['footers']) => (this.set.footers = value);
+	readonly rowSelectionColumnWidth = (value: RequiredSources<TData>['rowSelectionColumnWidth']) => (this.set.rowSelectionColumnWidth = value);
+	readonly theadRowHeight = (value: RequiredSources<TData>['theadRowHeight']) => (this.set.theadRowHeight = value);
+	readonly tbodyRowHeight = (value: RequiredSources<TData>['tbodyRowHeight']) => (this.set.tbodyRowHeight = value);
+	readonly tfootRowHeight = (value: RequiredSources<TData>['tfootRowHeight']) => (this.set.tfootRowHeight = value);
+	readonly columns = (value: RequiredSources<TData>['columns']) => (this.set.columns = value);
+	readonly footers = (value: RequiredSources<TData>['footers']) => (this.set.footers = value);
 	// ################################## END Set Sources ##################################################################
 
 	// ################################## BEGIN Properties ################################################################
 	// derived sources. kaynaklar/sources okunurken bu değişken kullanılacak. `table.get`
-	get = $derived({ ...this.defaultSources, ...this.set });
+	readonly get = $derived({ ...this.defaultSources, ...this.set });
 	// derived columns. kolon bilgileri okunurken bu değişken kullanılacak. `table.columns`
-	visibleColumns = $derived(
+	readonly visibleColumns = $derived(
 		this.get.columns
 			.map((col, index) => {
 				return {
@@ -74,25 +74,34 @@ class Table<TData extends Row> {
 
 	// ################################## BEGIN General Variables ######################################################
 	test = $state('test');
-	headerRowsCount = $state(1);
-	defaultOverscanThreshold = 4;
-	gridTemplateRows = $derived.by(() => {
+
+	private headerRowsCountState = $state(1);
+	readonly headerRowsCount = $derived(this.headerRowsCountState);
+	private setHeaderRowsCount = (value: number) => (this.headerRowsCountState = value);
+
+	private defaultOverscanThreshold = 4;
+
+	readonly gridTemplateRows = $derived.by(() => {
 		const repeatThead = this.headerRowsCount >= 1 ? `repeat(${this.headerRowsCount}, ${this.get.theadRowHeight}px)` : ``;
 		const repeatTbody = this.get.data.length > 0 ? `repeat(${this.get.data.length}, ${this.get.tbodyRowHeight}px)` : ``;
 		const repeatTfoot = this.get.footers.length > 0 ? `repeat(${this.get.footers.length}, ${this.get.tfootRowHeight}px)` : ``;
 		return `${repeatThead} ${repeatTbody} ${repeatTfoot}`;
 	});
-	gridTemplateColumns = $derived(`${this.get.rowSelection !== 'none' ? this.get.rowSelectionColumnWidth + 'px' : ''} ${this.visibleColumns.map((col) => col.width ?? `150px`).join(' ')}`);
+
+	readonly gridTemplateColumns = $derived(
+		`${this.get.rowSelection !== 'none' ? this.get.rowSelectionColumnWidth + 'px' : ''} ${this.visibleColumns.map((col) => col.width ?? `150px`).join(' ')}`
+	);
 	// ################################## END General Variables ########################################################
 
 	// ################################## BEGIN Events ##########################################################
 	// ***** onCellFocusChange Event *****
-	onCellFocusChange = (fn: OnCellFocusChange) => (this.onCellFocusChangeRun = fn);
+	readonly onCellFocusChange = (fn: OnCellFocusChange) => (this.onCellFocusChangeRun = fn);
 	private onCellFocusChangeRun?: OnCellFocusChange;
 	private onCellFocusChangeThis: OnCellFocusChange = (params) => {
 		if (this.onCellFocusChangeRun != null) this.onCellFocusChangeRun(params);
 	};
-	onRowSelectionChange = (fn: OnRowSelectionChange) => (this.onRowSelectionChangeRun = fn);
+	// ***** onRowSelectionChange Event *****
+	readonly onRowSelectionChange = (fn: OnRowSelectionChange) => (this.onRowSelectionChangeRun = fn);
 	private onRowSelectionChangeRun?: OnRowSelectionChange;
 	private onRowSelectionChangeThis: OnRowSelectionChange = (params) => {
 		if (this.onRowSelectionChangeRun != null) this.onRowSelectionChangeRun(params);
@@ -101,9 +110,9 @@ class Table<TData extends Row> {
 
 	// ################################## BEGIN Vertical Virtual Data ##################################################
 	private calculatingVirtualData = false;
-	virtualDataDerivedTrigger?: string = $state();
+	private virtualDataDerivedTrigger?: string = $state();
 	// derived virtualData. Virtual veriler okurken bu değişken kullanılacak. `table.data`
-	virtualData = $derived.by(() => {
+	readonly virtualData = $derived.by(() => {
 		if (this.get.enableVirtualization === false || this.element == null || this.virtualDataDerivedTrigger == null) return [];
 
 		const headerRowsHeight = this.headerRowsCount * this.get.theadRowHeight;
@@ -136,7 +145,7 @@ class Table<TData extends Row> {
 		return processedData;
 	});
 
-	setVirtualDataDerivedTrigger = async (virtualDataDerivedTrigger: string) => {
+	readonly setVirtualDataDerivedTrigger = async (virtualDataDerivedTrigger: string) => {
 		if (this.calculatingVirtualData) return;
 
 		this.calculatingVirtualData = true;
@@ -147,13 +156,13 @@ class Table<TData extends Row> {
 	// ################################## END Vertical Virtual Data ####################################################
 
 	// ################################## BEGIN Keyboard Navigation Methods ############################################
-	getPageUpRowIndex = () => {
+	readonly getPageUpRowIndex = () => {
 		const { rowVisibleStartIndex, currentHeight, dataRowHeight } = this.findVisibleRowIndexs({});
 		if (rowVisibleStartIndex == null || currentHeight == null || dataRowHeight == null) return undefined;
 
 		return rowVisibleStartIndex - Math.floor(currentHeight / dataRowHeight) + 1;
 	};
-	getPageDownRowIndex = () => {
+	readonly getPageDownRowIndex = () => {
 		const { rowVisibleEndIndex, currentHeight, dataRowHeight } = this.findVisibleRowIndexs({});
 		if (rowVisibleEndIndex == null || currentHeight == null || dataRowHeight == null) return undefined;
 
@@ -178,6 +187,7 @@ class Table<TData extends Row> {
 		this.setFocusedCell(focucedCell);
 		await tick();
 	};
+
 	private focusCellNode = () => {
 		const nextFocusedCellNode = this.element?.querySelector<HTMLDivElement>(':scope > [role="row"] > [data-focused="true"]');
 		if (nextFocusedCellNode == null) return;
@@ -187,7 +197,7 @@ class Table<TData extends Row> {
 		elementToFocus.focus({ preventScroll: true });
 	};
 
-	focusCell = async ({ cellToFocus, triggerVirtual = false }: { cellToFocus: Required<FocucedCell>; triggerVirtual?: boolean }) => {
+	readonly focusCell = async ({ cellToFocus, triggerVirtual = false }: { cellToFocus: Required<FocucedCell>; triggerVirtual?: boolean }) => {
 		await this.setFocusedCellTabIndex(cellToFocus); // tabindex durumunu ve focucedCell state'ini günceller.
 
 		// pageup veya pagedown gibi uzun atlamalar olduğunda, yani scan edilmiş tüm virtual datanın da uzağına gidilmek istendiğinde, virtual data bir kez güncellenir.
@@ -206,17 +216,17 @@ class Table<TData extends Row> {
 
 		this.onCellFocusChangeThis({ rowIndex: cellToFocus.rowIndex, colIndex: cellToFocus.colIndex });
 	};
-
 	// ################################## END Set Focused Cell State #####################################################
 
 	// ################################## BEGIN Row Selection Methods ##############################################################
-	private selectedRows: number[] = $state.raw([]); // Seçili satır indeksleri
-	readonly getSelectedRows = $derived(this.selectedRows); // reactive state getter
-	private setSelectedRows = (param: number[]) => (this.selectedRows = param);
-	private clearSelectedRows = () => (this.selectedRows = []);
+	private selectedRows: number[] = $state.raw([]); // Private state manager
+	readonly getSelectedRows = $derived(this.selectedRows); // Public readonly getter
+	private setSelectedRows = (param: number[]) => (this.selectedRows = param); // Private set method
+	private clearSelectedRows = () => (this.selectedRows = []); // Private clear methods
 
+	// Public methods with business logic
 	// Bir satırın seçimini değiştirir
-	toggleRowSelection = async (rowIndex: number) => {
+	readonly toggleRowSelection = async (rowIndex: number) => {
 		if (this.get.rowSelection === 'none') return;
 
 		const selectedRows = this.getSelectedRows;
@@ -235,23 +245,9 @@ class Table<TData extends Row> {
 		await tick();
 		this.onRowSelectionChangeThis({ selectedRows: this.getSelectedRows });
 	};
-	// Bir satırın seçimini değiştirir
-	/* toggleRowSelection = async (rowIndex: number) => {
-		if (this.get.rowSelection === 'none') return;
-
-		const selectedRows = this.getSelectedRows;
-		const index = selectedRows.indexOf(rowIndex);
-		if (index === -1) {
-			this.setSelectedRows([...selectedRows, rowIndex]); // Yeni seçim eklerken yeni bir array oluştur
-		} else {
-			this.setSelectedRows(selectedRows.filter((idx) => idx !== rowIndex)); // Seçimi kaldırırken filter kullan
-		}
-		await tick();
-		this.onRowSelectionChangeThis({ selectedRows: this.getSelectedRows });
-	}; */
 
 	// Tüm satırları seçer veya seçimi kaldırır
-	toggleAllRows = async (select: boolean) => {
+	readonly toggleAllRows = async (select: boolean) => {
 		if (this.get.rowSelection !== 'multiple') return;
 
 		if (select) {
@@ -265,7 +261,7 @@ class Table<TData extends Row> {
 	// ################################## END Row Selection Methods ################################################################
 
 	// ################################## BEGIN General Methods ##############################################################
-	getFooter = ({ field, foot }: { field: Field<TData>; foot: Footer<TData> }): number | string => {
+	readonly getFooter = ({ field, foot }: { field: Field<TData>; foot: Footer<TData> }): number | string => {
 		const footer = foot[field]; // sum, avg, count or footer content
 		if (footer == null) return '';
 
